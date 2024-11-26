7f62b86eec077553986a4de13f500163635f3cd2
Tiago Martins Reis <127058546+pmatjayme@users.noreply.github.com>
2024-11-26 00:05:28 -0300

    fim

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Aritmética',
+    'Quais são as operações básicas da Matemática?',
+    'Adição, Subtração, Multiplicação e Divisão.'
+)
+
+criaCartao(
+    'Função',
+    'O que seria o zero da função?',
+    'O valor onde o gráfico cruza o eixo da abscissa, ou ainda o valor de x para quando y for igual a 0.'
+)
+
+criaCartao(
+    'Estatística',
+    'Qual a definição de média aritmética?',
+    'Resultado da divisão do somatório dos números dados pela quantidade de números somados.'
+)
+
+criaCartao(
+    'Geometria',
+    'Como se calcula a área de um triângulo qualquer?',
+    'Multiplique a base pela altura e divida por dois.'
+)
+
+criaCartao(
+    'Conjuntos Numéricos',
+    'O que define o conjunto dos números racionais, dê exemplos?',
+    'Números que podem ser escritos na forma de fração. Por exemplo, 3/4, 10/2, -8/2'
+)
+
+criaCartao(
+    'Trigonometria',
+    'Quando utilizo o teorema de Pitágoras?',
+    'Para encontrar um dos lados de um triângulo retângulo, sendo dado os outros dois lados. h² = c² + c².'
+)
+
+criaCartao(
+    'Cálculo',
+    'Qual o valor de 58 - 2 x 10?',
+    '38.'
+)
+
+criaCartao(
+    'Razões Trigonométricas',
+    'Qual o valor de cosseno de 60º?',
+    '1/2.'
+)
+
+criaCartao(
+    'Ordem das Operações',
+    'Qual a ordem das operações matemática, da com maior para a menor prioridade?',
+    'Parênteses, Expoentes, Multiplicação e Divisão, Adição e Subtração.'
+)
